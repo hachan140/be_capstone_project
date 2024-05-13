392b7d1b035a26354d358fae72d1c3c4b406dad3
cuongle <pau30012002@gmail.com>
2024-05-14 00:54:37 +0700

    create_docker_file

--- src/cmd/public/main.go
+++ src/cmd/public/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	err := godotenv.Load("conf.example.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	BootstrapAndRun()
 }
