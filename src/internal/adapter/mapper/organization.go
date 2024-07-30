e69d2969b665d1757528fce267b6fb6db1e9bb3f
hachan140 <havu14102@gmail.com>
2024-07-30 21:13:43 +0700

    fix view category

--- src/internal/adapter/mapper/organization.go
+++ src/internal/adapter/mapper/organization.go
@@ -6,15 +6,19 @@ import (
 )
 
 func OrganizationModelToDTO(orgModel *model.Organization) *dtos.Organization {
+	percent := orgModel.DataUsed / orgModel.LimitData
 	return &dtos.Organization{
-		ID:          orgModel.ID,
-		Name:        orgModel.Name,
-		Description: orgModel.Description,
-		Status:      orgModel.Status,
-		IsOpenai:    orgModel.IsOpenai,
-		CreatedAt:   orgModel.CreatedAt,
-		CreatedBy:   orgModel.CreatedBy,
-		UpdatedAt:   orgModel.UpdatedAt,
-		UpdatedBy:   orgModel.UpdatedBy,
+		ID:              orgModel.ID,
+		Name:            orgModel.Name,
+		Description:     orgModel.Description,
+		Status:          orgModel.Status,
+		LimitData:       orgModel.LimitData,
+		DataUsed:        orgModel.DataUsed,
+		PercentDataUsed: percent,
+		IsOpenai:        orgModel.IsOpenai,
+		CreatedAt:       orgModel.CreatedAt,
+		CreatedBy:       orgModel.CreatedBy,
+		UpdatedAt:       orgModel.UpdatedAt,
+		UpdatedBy:       orgModel.UpdatedBy,
 	}
 }
--- src/internal/core/dtos/organization.go
+++ src/internal/core/dtos/organization.go
@@ -3,13 +3,16 @@ package dtos
 import "time"
 
 type Organization struct {
-	ID          uint      `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Status      int       `json:"status"`
-	IsOpenai    bool      `json:"is_openai"`
-	CreatedAt   time.Time `json:"created_at"`
-	CreatedBy   string    `json:"created_by"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	UpdatedBy   string    `json:"updated_by"`
+	ID              uint      `json:"id"`
+	Name            string    `json:"name"`
+	Description     string    `json:"description"`
+	Status          int       `json:"status"`
+	IsOpenai        bool      `json:"is_openai"`
+	LimitData       int64     `json:"limit_data"`
+	DataUsed        int64     `json:"data_used"`
+	PercentDataUsed int64     `json:"percent_data_used"`
+	CreatedAt       time.Time `json:"created_at"`
+	CreatedBy       string    `json:"created_by"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	UpdatedBy       string    `json:"updated_by"`
 }
